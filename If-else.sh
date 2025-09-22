0953c25819f79606f47a1ea3d2a41cf7a64a83b3
Ramya <ramyasvpp94@gmail.com>
2025-09-22 09:30:07 -0700

    if-else.sh

--- If-else.sh
+++ If-else.sh
@@ -8,6 +8,6 @@ if [ $((number % 2)) -eq 0 ]; then
 else
     echo "$number is an odd number."
 fi
-save this script in a file (e.g., check_even_odd.sh), give it execute permissions using chmod +x check_even_odd.sh, and run it using ./check_even_odd.sh.
+
 
          
