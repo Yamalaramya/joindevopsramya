20890e29771208bbdc1fbbeb2b1e361adce169b9
Ramya <ramyasvpp94@gmail.com>
2025-09-22 07:59:03 -0700

    if-else.sh

--- If-else.sh
+++ If-else.sh
@@ -2,7 +2,7 @@
 echo " Please enter the number "
 read NUMBER
 if [ $(($NUMBER % 2)) -eq 0] then
-    echo " given number $NUMBER is even "
+    echo "given number $NUMBER is even"
 else
-    echo " Given number $NUMBER is odd "
+    echo "Given number $NUMBER is odd "
 fi         
