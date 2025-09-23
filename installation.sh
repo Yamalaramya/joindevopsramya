dfb0c612e0747a26b6ff4914e9fc6d4d32a27fc1
Ramya <ramyasvpp94@gmail.com>
2025-09-23 08:02:14 -0700

    installation.sh

--- installation.sh
+++ installation.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [$USERID -ne 0]; then
 
     echo "error:: please run this script using root privaliges"
+    exit 1
 fi
 
 dnf install mysql -y
@@ -12,6 +13,7 @@ dnf install mysql -y
 if [$? -ne 0];then
 
     echo "error:: Installing mysql is failing"
+    exit 1
 
 else
     echo "installing mysql is success"
